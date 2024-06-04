6412f709f9e0245c30c8924cf8e5dc70eecb2619
mikepat12 <mikepat12@gmail.com>
2024-06-04 10:36:23 -0400

    Finalized sports csv file. Added test code. Added R shiny file.

--- code_sandbox.R
+++ code_sandbox.R
@@ -7,6 +7,7 @@
 library(readxl)
 library(zipcodeR)
 library(leaflet)
+library(leaflet.providers)
 library(tidyverse)
 
 
@@ -33,9 +34,11 @@ merged_2data <- merged_1data %>%
     mutate(lat.x = coalesce(lat.x, lat.y),
            lng.x = coalesce(lng.x, lng.y)) %>%
     rename('lat' = lat.x, 'lng' = lng.x) %>%
-    select(-lat.y, -lng.y)
-# Need to manually add in Mexico lat/lng. Only 1 team there
+    select(-lat.y, -lng.y, -image_link)
 
+# Manually add in Mexico lat/lng. Only 1 team there in G League
+merged_2data[79, 10] <- 19.4
+merged_2data[79, 11] <- -99.2
 
 # Next steps: Get image links to post and customize how the map looks in code below
 # After: Move to next sport
@@ -49,6 +52,25 @@ merged_2data <- merged_1data %>%
 ###########################################################
 # Creating Map
 ###########################################################
+test <- merged_2data %>% # only use this code when trying icons to filter. 
+    filter(sport == 'Baseball',
+           league == 'Single A')
+icons <- makeIcon(
+    iconUrl = test$link2,
+    iconWidth = 31 * 215/230, iconHeight = 31,
+    iconAnchorX = 31 * 215/230/2, iconAnchorY = 16
+)
+test %>%
+    leaflet() %>%
+    addTiles() %>%
+    addMarkers(~ lng, ~ lat, popup = ~ team_name, icon = icons)
+
+
+merged_2data <- merged_2data %>% # only use this code when trying icons to filter. 
+    filter(sport == 'Hockey')
+merged_2data <- merged_2data %>% 
+    filter(league == 'AHL')
+
 icons <- makeIcon(
     iconUrl = merged_2data$link2,
     iconWidth = 31 * 215/230, iconHeight = 31,
@@ -56,20 +78,18 @@ icons <- makeIcon(
 )
 
 merged_2data %>%
-    filter(league == 'NFL') %>%
     leaflet() %>%
     addTiles() %>%
     addMarkers(~ lng, ~ lat, popup = ~ team_name, icon = icons)
 
-
-# Use test to try other image types. Popup format only works for unfiltered dataset
-test <- merged_2data %>%
-    filter(merged_2data$league == 'NFL')
+# HTML popup code needs full data. Cannot use filtered dataset
 merged_2data %>%
     leaflet() %>%
     addTiles() %>%
+    addProviderTiles("CartoDB.DarkMatter") %>%
     addMarkers(~ lng, ~ lat, icon = icons, 
-               popup = paste(merged_2data$team_name, '<br>', merged_2data$stadium, '<br>')) 
+               popup = paste(merged_2data$team_name, '<br>', merged_2data$stadium, '<br>', 
+                             'Affiliate:', merged_2data$affiliate, '<br>'))
 
 
 
